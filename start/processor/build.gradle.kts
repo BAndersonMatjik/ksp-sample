e604f0f1ce157cc2fe556772af3860c0f8ae4e26
Billy Anderson Matjik <billy.matjik@anteraja.id>
2022-05-18 08:43:45 +0700

    Init KSP

--- start/processor/build.gradle.kts
+++ start/processor/build.gradle.kts
@@ -21,6 +21,11 @@ dependencies {
 
     implementation("com.squareup:kotlinpoet:1.10.1")
     implementation("com.squareup:kotlinpoet-ksp:1.10.1")
+
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.4.32")
+    testImplementation("com.github.tschuchortdev:kotlin-compile-testing:1.4.4")
+    testImplementation("com.github.tschuchortdev:kotlin-compile-testing-ksp:1.4.4")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
---
+++ start/processor/src/test/kotlin/com/bmatjik/processor/provider/FieldToListProcessorTest.kt
@@ -0,0 +1,81 @@
+package com.bmatjik.processor.provider
+
+import com.tschuchort.compiletesting.KotlinCompilation
+import com.tschuchort.compiletesting.SourceFile
+import com.tschuchort.compiletesting.symbolProcessorProviders
+import org.intellij.lang.annotations.Language
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import java.io.File
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+
+class FieldToListProcessorTest {
+
+    @Rule
+    @JvmField
+    var temporaryFolder: TemporaryFolder = TemporaryFolder()
+
+    @Test
+    fun `target is not a data class`() {
+        val kotlinSource = SourceFile.kotlin(
+            "file1.kt", """
+        package com.tests.summable
+        
+        import com.bmatjik.annotations.Listed
+
+          data class FooSummable(
+            @Listed
+            val bar: Int = 234,
+            @Listed
+            val baz: Int = 123
+          )
+    """
+        )
+        val compilationResult = compile(kotlinSource)
+
+        println("CHECK ${compilationResult.messages}")
+
+        assertEquals(KotlinCompilation.ExitCode.COMPILATION_ERROR, compilationResult.exitCode)
+        val expectedMessage = "@Listed must target a data class."
+        assertTrue("Expected message containing text $expectedMessage but got: ${compilationResult.messages}") {
+            compilationResult.messages.contains(expectedMessage)
+        }
+    }
+
+    private fun assertSourceEquals(@Language("kotlin") expected: String, actual: String) {
+        assertEquals(
+            expected.trimIndent(),
+            // unfortunate hack needed as we cannot enter expected text with tabs rather than spaces
+            actual.trimIndent().replace("\t", "    ")
+        )
+    }
+
+    private fun compile(vararg source: SourceFile) = KotlinCompilation().apply {
+        sources = source.toList()
+        symbolProcessorProviders = listOf(FieldToListProcessorProvider())
+        workingDir = temporaryFolder.root
+        inheritClassPath = true
+        verbose = false
+    }.compile()
+
+    private fun KotlinCompilation.Result.sourceFor(fileName: String): String {
+        return kspGeneratedSources().find { it.name == fileName }
+            ?.readText()
+            ?: throw IllegalArgumentException("Could not find file $fileName in ${kspGeneratedSources()}")
+    }
+
+    private fun KotlinCompilation.Result.kspGeneratedSources(): List<File> {
+        val kspWorkingDir = workingDir.resolve("ksp")
+        val kspGeneratedDir = kspWorkingDir.resolve("sources")
+        val kotlinGeneratedDir = kspGeneratedDir.resolve("kotlin")
+        val javaGeneratedDir = kspGeneratedDir.resolve("java")
+        return kotlinGeneratedDir.walk().toList() +
+                javaGeneratedDir.walk().toList()
+    }
+
+    private val KotlinCompilation.Result.workingDir: File
+        get() = checkNotNull(outputDirectory.parentFile)
+}
